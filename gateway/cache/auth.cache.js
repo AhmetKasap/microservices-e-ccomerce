b383a720bda9780de2e6f2a3cf06336ab2caddc3
ahmetkasap <aahmetkasap@gmail.com>
2024-08-11 11:42:41 +0300

    added docker.yaml to stand up all servers

--- gateway/cache/auth.cache.js
+++ gateway/cache/auth.cache.js
@@ -3,18 +3,13 @@ const redis = require('redis')
 const client = redis.createClient()
 client.connect() //redis connection
 
-
-
 const getCache = async (userId) => {
 
     const foundUserId = await client.sIsMember("userIdList", userId)
     
     if(!foundUserId) return null
     else return foundUserId
-
 }
-
-
 
 
 module.exports = {
